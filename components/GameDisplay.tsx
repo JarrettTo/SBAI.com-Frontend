da5ae79d723e3fca728482c2387ddb36756e29da
sleep-deepfried <pulidoearljohn14@gmail.com>
2024-02-06 01:31:59 +0800

    fixed the adropdown box and populated the menu inputs

--- components/GameDisplay.tsx
+++ components/GameDisplay.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { INBAGame} from '../types/Game';
 import * as NBAIcons from 'react-nba-logos';
 import { Button } from '@mui/material';
-const GameDisplay: React.FC<INBAGame> = (props) =>{
-   const {id, homeTeam, homeTeamLogo, awayTeam, awayTeamLogo, schedule} = props;
+
+interface GameDisplayProps extends INBAGame {
+    isInDropdown?: boolean;
+  }
+
+const GameDisplay: React.FC<GameDisplayProps> = (props) =>{
+   const {id, homeTeam, homeTeamLogo, awayTeam, awayTeamLogo, schedule,  isInDropdown = false,} = props;
    const teamIconMap = {
         //TODO: Complete for All NBA Teams
         "TOR": NBAIcons.TOR,
@@ -50,37 +55,36 @@ const GameDisplay: React.FC<INBAGame> = (props) =>{
         //TODO: Either redirect/dropdown and show predictions of AI Model from API
     }
    return(
-    <div style={{display: 'flex', flexDirection:'row', justifyContent: 'space-between', marginTop: '20px' }}>
+    <div style={{display: 'flex', flexDirection:'row', justifyContent: 'space-between', marginTop: '20px', }}>
     
         <div style={{marginLeft: '30px', display: 'flex', flexDirection: 'row', alignItems: 'center', width:'60%',justifyContent: 'space-between'}}>
-            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', width:'33%'}}>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
                 <div>{getTeamIcon(teamAbbMap[homeTeam])}</div>
                 <div style={{display: 'flex', flexDirection: 'column', alignItems: 'start', marginLeft: '10px'}}>
                     <p style={{fontSize:'20px', fontWeight:'800', color: "black"}}>{teamAbbMap[homeTeam]}</p>
 
                     <p style={{fontSize:'14px', fontWeight:'400'}}>{homeTeam}</p>
 
-                    <p style={{fontSize:'14px', fontWeight:'400'}}>(Home)</p>
                 </div>
                 
             </div>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width:'33%'}}>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                 <p style={{fontSize:'20px', fontWeight:'800', color: "black"}}>VS</p>
                 <p style={{fontSize:'14px', fontWeight:'400'}}>{formatTime(schedule)}</p>
             </div>
-            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', width:'33%', justifyContent:'end'}}>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
                 
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'end', marginRight: '10px'}}>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'end',  marginRight: '10px'}}>
                     <p style={{fontSize:'20px', fontWeight:'800', color: "black"}}>{teamAbbMap[awayTeam]}</p>
 
                     <p style={{fontSize:'14px', fontWeight:'400'}}>{awayTeam}</p>
 
-                    <p style={{fontSize:'14px', fontWeight:'400'}}>(Away)</p>
                 </div>
                 <div>{getTeamIcon(teamAbbMap[awayTeam])}</div>
                 
             </div>
         </div>
+        {isInDropdown ? null : (
         <div style={{display:'flex', flexDirection:'row', alignItems: 'center', marginRight: '30px'}}>
             <Button
                 variant="contained"
@@ -128,8 +132,9 @@ const GameDisplay: React.FC<INBAGame> = (props) =>{
                 Predict
             </Button>
         </div>
+        )}
     </div>
-   )
+    )
 
 }
 export default GameDisplay;
