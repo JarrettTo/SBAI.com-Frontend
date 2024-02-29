7f0fd28a159ac10bb985a3b23dde84b61411ce32
GithubSim13 <144484430+GithubSim13@users.noreply.github.com>
2024-02-29 21:04:11 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import { Odds } from '../types/Odds';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import { Predictions } from "../types/Predictions";
+import { Slider } from "@mui/material";
 
 dotenv.config();
 
@@ -175,81 +176,10 @@ const HomePage = () => {
       
     
     return (
-        
-        <div style={{display : "flex", flexDirection : "column", alignItems:"center", height: "100vh", overflow: 'hidden'}} >
+        <div style={{display : "flex", flexDirection : "column", alignItems:"center", height: "200vh", overflow: 'auto'}} >
             <div style={{display : "flex", flexDirection : "column",alignItems:"center",justifyContent:"center", marginTop: '105px'}}>
                 <h1 style={{fontWeight: 700, fontSize:'80px'}}>Sports Betting AI</h1>
                 <p style={{fontWeight: 500, fontSize:'15px', width: '30vw', textAlign:'center', marginBottom: '30px'}}>Select an upcoming NBA game and click the predict button to generate a prediciton using our latest AI Model!</p>
-                {/*<div>
-                    <Button
-                        variant="contained"
-                        className="select-game-button"
-                        endIcon={<ArrowDropDownIcon />}
-                        onClick={handleClick}
-                        style={{
-                            fontSize:'20px',
-                            width: '50vw',
-                            backgroundColor: '#fff', // Set the background color
-                            color: '#aaa', // Set the text color to gray
-                            borderRadius: '10px', // Rounded corners
-                            textTransform: 'none', // Avoid uppercase transformation
-                            marginRight:'30px',
-                            whiteSpace: 'pre-line',
-                        }}
-                    >
-                        {selectedGame
-                            ? `${selectedGame.homeTeam} vs ${selectedGame.awayTeam} \n${formatTime(selectedGame.schedule)}`
-                            : 'Select an NBA Game'}
-                    </Button>
-                
-                    <Menu
-                        id="simple-menu"
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        MenuListProps={{
-                            'aria-labelledby': 'basic-button',
-                        }}
-                        PaperProps={{
-                            style: {
-                                borderRadius: '10px', // Rounded corners
-                                width: '50vw',
-                                
-                            },
-                        }}
-                    >
-                        {gameSchedules.map((game) => (
-                            <MenuItem key={game.id} onClick={() => handleGameSelect(game)} style={{justifyContent:'center'}}>
-                                <GameDisplay
-                                    id={game.id}
-                                    homeTeam={game.homeTeam}
-                                    homeTeamLogo={game.homeTeamLogo}
-                                    awayTeam={game.awayTeam}
-                                    awayTeamLogo={game.awayTeamLogo}
-                                    schedule={game.schedule}
-                                    isInDropdown={true}
-                                    odds={gameOdds.find((odds) => odds.home_team === game.homeTeam)}
-                                    predictions={gamePreds.find((odds) => odds.home_team === game.homeTeam)}
-                                />
-                            </MenuItem>
-                        ))}
-                    </Menu>
-                    <Button
-                        variant="contained"
-                        className="predict-button"
-                        onClick={handleClick}
-                        style={{
-                            fontSize:'20px',
-                            width: '10vw',
-                            backgroundColor: '#068FFF', // Set the background color
-                            color: '#EEEEEE', // Set the text color to gray
-                            borderRadius: '10px', // Rounded corners
-                            textTransform: 'none', // Avoid uppercase transformation
-                        }}
-                    >
-                        Predict
-                    </Button>
-                    </div>*/}
             </div>
             <div
                 style={{
