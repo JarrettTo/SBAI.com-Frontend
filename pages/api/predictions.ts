d7031928402ee4fcabf73a2b17a879124d581d01
crlse <129936567+josecarlosregala@users.noreply.github.com>
2024-03-10 17:48:03 +0800

    Added mapping for LA Clippers

--- pages/api/predictions.ts
+++ pages/api/predictions.ts
@@ -8,6 +8,10 @@ dotenv.config();
 // Base URL for the Sportsradar API
 const BASE_URL = 'http://3.101.18.227:8080/predict';
 
+const teamNameMap: Record<string, string> = {
+  'LA Clippers': 'Los Angeles Clippers',
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const response = await axios.get(`${BASE_URL}`);
@@ -15,9 +19,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // Map the response data to the INBAGame interface
     const predictions: Predictions[] = response.data.map((game: any) => ({
         id: game.id,
-        home_team: game.home_team,
-        away_team: game.away_team,
-        ml_pred: game.ml_pred,
+        home_team: teamNameMap[game.home_team] || game.home_team,
+        away_team: teamNameMap[game.away_team] || game.away_team,
+        ml_pred: teamNameMap[game.ml_pred] || game.ml_pred,
         ml_conf: game.ml_conf,
         ou_pred: game.ou_pred,
         ou_conf: game.ou_conf
