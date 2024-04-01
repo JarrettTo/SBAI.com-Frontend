7a161707dc81b36a7e6c50d50ecc72cf34892bbc
sleep-deepfried <pulidoearljohn14@gmail.com>
2024-04-01 05:16:36 -0500

    updated the preditction db

--- pages/api/recordPredictions.ts
+++ pages/api/recordPredictions.ts
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     let connection;
     const url = process.env.URL;
     const gamePredictions = [
-        {"away_team":"Golden State Warriors","home_team":"Miami Heat","id":"0","ml_conf":"60.4%","ml_pred":"Miami Heat","ou_conf":"61.5%","ou_pred":"OVER 218.5"},{"away_team":"Los Angeles Lakers","home_team":"Milwaukee Bucks","id":"1","ml_conf":"72.4%","ml_pred":"Milwaukee Bucks","ou_conf":"71.7%","ou_pred":"UNDER 232"},{"away_team":"Oklahoma City Thunder","home_team":"New Orleans Pelicans","id":"2","ml_conf":"51.1%","ml_pred":"New Orleans Pelicans","ou_conf":"59.5%","ou_pred":"UNDER 223.5"},{"away_team":"Dallas Mavericks","home_team":"Sacramento Kings","id":"3","ml_conf":"55.0%","ml_pred":"Sacramento Kings","ou_conf":"67.7%","ou_pred":"UNDER 236"}
+        {"away_team":"Cleveland Cavaliers","home_team":"Denver Nuggets","id":"0","ml_conf":"64.8%","ml_pred":"Denver Nuggets","ou_conf":"53.3%","ou_pred":"UNDER 215.5"},{"away_team":"Los Angeles Lakers","home_team":"Brooklyn Nets","id":"1","ml_conf":"60.3%","ml_pred":"Los Angeles Lakers","ou_conf":"54.9%","ou_pred":"OVER 226.5"},{"away_team":"LA Clippers","home_team":"Charlotte Hornets","id":"2","ml_conf":"72.2%","ml_pred":"LA Clippers","ou_conf":"59.1%","ou_pred":"UNDER 214.5"},{"away_team":"Philadelphia 76ers","home_team":"Toronto Raptors","id":"3","ml_conf":"61.5%","ml_pred":"Philadelphia 76ers","ou_conf":"57.4%","ou_pred":"OVER 218.5"},{"away_team":"Miami Heat","home_team":"Washington Wizards","id":"4","ml_conf":"73.0%","ml_pred":"Miami Heat","ou_conf":"56.4%","ou_pred":"OVER 217.5"},{"away_team":"Oklahoma City Thunder","home_team":"New York Knicks","id":"5","ml_conf":"50.9%","ml_pred":"Oklahoma City Thunder","ou_conf":"55.7%","ou_pred":"UNDER 215"},{"away_team":"Dallas Mavericks","home_team":"Houston Rockets","id":"6","ml_conf":"50.1%","ml_pred":"Dallas Mavericks","ou_conf":"64.6%","ou_pred":"OVER 230.5"},{"away_team":"Chicago Bulls","home_team":"Minnesota Timberwolves","id":"7","ml_conf":"73.8%","ml_pred":"Minnesota Timberwolves","ou_conf":"59.6%","ou_pred":"OVER 214"},{"away_team":"Golden State Warriors","home_team":"San Antonio Spurs","id":"8","ml_conf":"60.0%","ml_pred":"Golden State Warriors","ou_conf":"54.6%","ou_pred":"OVER 227"},{"away_team":"Utah Jazz","home_team":"Sacramento Kings","id":"9","ml_conf":"69.2%","ml_pred":"Sacramento Kings","ou_conf":"54.8%","ou_pred":"OVER 222"}
     ];
     try {
         const response = await axios.get(`${url}today_games`, {
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
 
             // Insert into Games table
             const gameQuery = `
-                INSERT INTO Games (id, homeTeam, awayTeam, schedule)
+                INSERT INTO Games (id, home_team, away_team, schedule)
                 VALUES (?, ?, ?, ?)
             `;
             await connection.execute(gameQuery, [game.id, game.homeTeam, game.awayTeam, mysqlTimestamp]);
