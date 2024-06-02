0efa11e85bbd506f873c90eb37faae0588bc231a
JarrettTo <justin.jarrett.to@gmail.com>
2024-06-02 17:13:00 +0800

    Generate Predictions and Setup Cron

---
+++ pages/api/mlb/predict/model.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { IMLBGame } from '../../../../types/Game';
+import dotenv from 'dotenv';
+import { addDays } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
+import { Predictions } from 'types/Predictions';
+dotenv.config();
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// Base URL for the Sportsradar API
+const BASE_URL = 'https://api.sportradar.com';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+
+    const response = await axios.get(`http://54.176.127.3:4000/predict`);
+    const predictions: Predictions[] = response.data
+    console.log("Predictions:", predictions)
+    res.status(200).json(predictions); 
+  } catch (error) {
+    console.error('Error fetching game predictions:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
